d769e03d38e081871c8287a36b22df7e7fb93484
buildbro <apeapius@gmail.com>
2019-01-31 16:14:14 +0100

    Add essential UI elements EditText, Buttons, etc

--- app/src/main/java/com/example/firebaseemailpasswordexample/MainActivity.java
+++ app/src/main/java/com/example/firebaseemailpasswordexample/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.firebaseemailpasswordexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button registerBtn, loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
     }
 }
